f5edefcc40eaa8a2af9fb0410503aa068728d4f6
zhangzhen <zhangzhen2540@qq.com>
2016-02-08 01:46:37 +0800

    Story 182: Fix benchmarks for speed

---
+++ src/main/java/indi/zhz/web/HelloWorld.java
@@ -0,0 +1,8 @@
+package indi.zhz.web;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello, world.");
+		System.out.println("ok");
+	}
+}
---
+++ src/test/inid/zhz/web/HelloWorldTest.java
@@ -0,0 +1,8 @@
+package indi.zhz.web;
+
+public class HelloWorldTest {
+	public static void main(String[] args) {
+		System.out.println("hello, world. Test run");
+		System.out.println("update by zz");
+	}
+}
